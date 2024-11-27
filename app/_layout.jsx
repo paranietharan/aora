20351d727e659090a470697d7dadeb1101374bc7
paranietharan <paranie20@gmail.com>
2024-11-27 19:29:01 +0530

    Basic fonts updated finished

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import { Slot } from 'expo-router'
+// import "../global.css";
+
+// const RootLayout = () => {
+//   return <Slot />
+// }
+
+// export default RootLayout
+import React, { useEffect } from 'react';
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router'
 import "../global.css";
 
-const RootLayout = () => {
-  return <Slot />
-}
+SplashScreen.preventAutoHideAsync();
 
-export default RootLayout
+const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="(tabs)" options={{ title: "Tabs" }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
