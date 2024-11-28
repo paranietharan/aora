1760532bf78949a84af82fbf1147fbbae2f098b3
paranietharan <paranie20@gmail.com>
2024-11-28 17:32:02 +0530

    signup finished

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,35 +1,67 @@
-import { View, Text, ScrollView, Image } from 'react-native';
-import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 
-import images from '../../constants/images';
-import FormField from '../../components/FormField';
+import { images } from "../../constants";
+import { createUser } from "../../lib/appwrite";
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import FormField from '../../components/FormField';
+// import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignUp = () => {
+  // const { setUser, setIsLogged } = useGlobalContext();
 
+  const [isSubmitting, setSubmitting] = useState(false);
   const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: ''
+    username: "",
+    email: "",
+    password: "",
   });
-  
-  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      // setUser(result);
+      // setIsLogged(true);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ justifyContent: 'center' }} className="w-full px-4 my-6">
-        <View className="min-h-[85vh] flex justify-center">
+      <ScrollView>
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
 
-          <Image source={images.logo} className="w-24 h-24" resizeMode='contain' />
-          <Text className="text-2xl text-white text-psemibold mt-10 font-psemibold">Sign Up to Aora</Text>
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Sign Up to Aora
+          </Text>
 
           <FormField
             title="Username"
             value={form.username}
             handleChangeText={(e) => setForm({ ...form, username: e })}
-            otherStyles="mt-7"
+            otherStyles="mt-10"
           />
 
           <FormField
@@ -49,14 +81,21 @@ const SignUp = () => {
 
           <CustomButton
             title="Sign Up"
+            handlePress={submit}
             containerStyles="mt-7"
-            handlePress={() => console.log(form)}
             isLoading={isSubmitting}
           />
 
-          <View className="justify-center pt-5 flex-row gap-2">
-            <Text className="text-white text-sm">Have an account? already</Text>
-            <Link href="/sign-in" className="text-secondary text-sm font-pbold">Sign In</Link>
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Login
+            </Link>
           </View>
         </View>
       </ScrollView>
